a52a896cde093148ab51ef56249d5965228cc0be
SamyakBhoyar <74291373+SamyakBhoyar@users.noreply.github.com>
2021-02-01 10:40:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,91 +1,42 @@
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
 
-car1_width = 120;
-car1_height = 70;
-car1_image = "https://i.postimg.cc/9rqYz9HM/car1.png";
-car1_x = 10;
-car1_y = 10;
+car1_width=150;
+car1_height=90;
+car1_img="https://i.postimg.cc/YqdnnNX1/car1.png";
+car1_x=10;
+car1_y=10;
 
-car2_width = 120;
-car2_height = 70;
-car2_image = "https://i.postimg.cc/tnnW1Kff/car2.png";
-car2_x = 10;
-car2_y = 100;
+car2_width=150;
+car2_height=90;
+car2_img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyFVvLJrD5dLAWPywtsCgeTcdK_eBs73As4A&usqp=CAU";
+car2_x=10;
+car2_y=100;
 
-background_image = "https://i.postimg.cc/bv5d35nK/racing.jpg";
+background_image="https://i.postimg.cc/bv5d35nK/racing.jpg";
 
 function add() {
-	background_imgTag = new Image(); 
-	background_imgTag.onload = uploadBackground; 
-	background_imgTag.src = background_image;   
+    background_imgtag=new Image();
+    background_imgtag.onload=uploadbackground;
+    background_imgtag.src=background_image;
 
-	car1_imgTag = new Image(); 
-	car1_imgTag.onload = uploadcar1; 
-	car1_imgTag.src = car1_image;   
+    car1_imgtag=new Image();
+    car1_imgtag.onload=uploadcar1;
+    car1_imgtag.src=car1_img;
 
-	car2_imgTag = new Image(); 
-	car2_imgTag.onload = uploadcar2;
-	car2_imgTag.src = car2_image;
+    car2_imgtag=new Image();
+    car2_imgtag.onload=uploadcar2;
+    car2_imgtag.src=car2_img;
 }
 
-function uploadBackground() {
-	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+function uploadbackground() {
+    ctx.drawImage(background_imgtag, 0, 0, canvas.width, canvas.height);
 }
 
 function uploadcar1() {
-	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+    ctx.drawImage(car1_imgtag, car1_x, car1_y, car1_width, car1_height);
 }
+
 function uploadcar2() {
-	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
+    ctx.drawImage(car2_imgtag, car2_x, car2_y, car2_width, car2_height);
 }
-
-
-window.addEventListener("keydown", my_keydown);
-
-function my_keydown(e)
-{
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-		if(keyPressed == '38')
-		{
-			car1_up();
-			console.log("up arrow key");
-		}
-		if(keyPressed == '40')
-		{
-			car1_down();
-			console.log("down arrow key");
-		}
-		if(keyPressed == '37')
-		{
-			car1_left();
-			console.log("left arrow key");
-		}
-		if(keyPressed == '39')
-		{
-			car1_right();
-			console.log("right arrow key");
-		}
-		if(keyPressed == '87')
-		{
-			car2_up();
-			console.log("key w");
-		}
-		if(keyPressed == '83')
-		{
-			car2_down();
-			console.log("key s");
-		}
-		if(keyPressed == '65')
-		{
-			car2_left();
-			console.log("key a");
-		}
-		if(keyPressed == '68')
-		{
-			car2_right();
-			console.log("key d");
-		}
-}
-
